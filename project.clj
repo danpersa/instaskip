605191c3d41e077611e204027ee103fbec487bc2
Dan Persa <dan.persa@gmail.com>
2016-03-31 18:21:17 +0200

    Try to transform to json

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [instaparse          "1.4.1"]
-                 [rhizome             "0.2.5"]]
+  :dependencies [[org.clojure/clojure   "1.8.0"]
+                 [instaparse            "1.4.1"]
+                 [rhizome               "0.2.5"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot instaskip.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/instaskip/core.clj
+++ src/instaskip/core.clj
@@ -1,13 +1,15 @@
 (ns instaskip.core
-  (:require [instaparse.core :as insta])
+  (:require [instaparse.core :as insta]
+            [clojure.data.json :as json])
   (:gen-class))
 
 (def eskip
   (insta/parser
     "
-     eskip-file       = eskip+
-     eskip            = name <':'> predicates <'->'> (filter <'->'> )* backend <';'>
+     routes           = route+
+     route            = name <':'> predicates <'->'> filters backend <';'>
      name             = #'[a-zA-Z0-9]+'
+     filters          = (filter <'->'> )*
      predicates       = predicate? (<'&&'> predicate)*
      predicate        = predicate-name <'('> predicate-arg? (<','> predicate-arg)* <')'>
      predicate-name   = #'[a-zA-Z0-9]*'
@@ -22,20 +24,46 @@
      number           = #'[0-9]+\\.[0-9]*'
      regexval         = #'\\/\\^[^$]*\\$/'
      "
-    :auto-whitespace :standard))
+    :auto-whitespace :standard
+    :output-format :hiccup))
 
+(defn str->num [str]
+  (let [n (read-string str)]
+    (if (number? n) n nil)))
 
-(let [eskip-route "
+(defn call-eskip []
+  (let [eskip-route "
                    hello: predicate1(/^.*$/) && predicate2(\"arg1\", 4.3)
                    -> filter1(\"arg1\")
                    -> filter2(\"arg1\", 4.3, \"arg2\")
                    -> filter3()
-                   -> <shunt>;
-                   hello1: <shunt>;"]
-  (defn -main
-    "I don't do a whole lot ... yet."
-    [& args]
-    (println "Hello, Instaskip!")
-    (println (eskip eskip-route))
-    (insta/visualize
-      (eskip eskip-route))))
+                   -> \"https://hello.com\";
+                   hello1: -> <shunt>;"]
+   (eskip eskip-route)))
+
+(defn name-and-args-to-map [name & args] {:name name :args (vec args)})
+(defn args-to-map [name & values] (hash-map name (vec values)))
+
+(insta/transform
+  {:shunt          (fn [sh] "")
+   :string         identity
+   :number         str->num
+   :filter-name    identity
+   :filter-arg     identity
+   :filter         name-and-args-to-map
+   :filters        (partial args-to-map :filters)
+   :predicate-name identity
+   :predicate-arg  identity
+   :predicate      name-and-args-to-map
+   }
+  (call-eskip))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, Instaskip!")
+  (println
+    (eskip eskip-route))
+  ;(insta/visualize
+  ;  (eskip eskip-route))
+  )
