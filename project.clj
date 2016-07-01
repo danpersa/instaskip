25c63157c6120a2f8d1c2c95986e124e6f0b2bc4
Dan Persa <dan.persa@gmail.com>
2016-07-01 10:26:23 +0200

    Release 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.danpersa/instaskip "0.2.9"
+(defproject org.clojars.danpersa/instaskip "0.3.0"
   :description "Transforms from eskip to json to eskip."
   :url "https://github.com/danpersa/instaskip"
   :license {:name "MIT License"
